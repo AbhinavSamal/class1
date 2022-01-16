43e737b14fd8570192fc29239d01d1a65c527976
AbhinavSamal <abhinav.samal.4594@gmail.com>
2022-01-16 16:35:57 +0530

    first commit

---
+++ Class 1.py
@@ -0,0 +1,39 @@
+#Data Types
+#Strings - Text - "Abhinav"
+#Integers - Number - 1,2,3
+#Float/Double - Decimal - 1.3, 4.5, 9.0
+
+#Variable
+a = 9.43
+b = 'Yolo'
+c = 8
+
+#Print
+print("Hello I am print statement")
+print(f'B variable stores: {b}')
+print('c has : {} and a has: {}'.format(c, a))
+
+#Input
+name = input("what is your name?")
+print(f"Hi, {name}")
+
+#Conditional Statement
+number = int(input('Enter a number: '))
+
+#if-else(Odd & even)
+if number % 2 == 0:
+    print(f'{number} is even')
+else:
+    print(f'{number} is odd')
+
+#if-elif-else(Drive a car)
+if number > 18:
+    print('You can drive a car.')
+elif number < 18:
+    print('Do not drive a car.')
+else:
+    print("You should learn how to drive a car.")
+
+#Loops
+#Coming soon
+
